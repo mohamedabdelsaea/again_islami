f8a66e00d382d0344e4b2f9a94bf2eab2982bc11
m7md-abdelsame3 <mmdabdo94@gmail.com>
2025-07-14 10:58:40 +0300

    create app again

--- lib/moduels/splash/splash_screen.dart
+++ lib/moduels/splash/splash_screen.dart
@@ -1,7 +1,30 @@
+import 'dart:async';
+
+import 'package:again_islami/core/routes/page_route_name.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          PageRouteName.layout,
+          (route) => false,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +48,7 @@ class SplashScreen extends StatelessWidget {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: size.height*0.1),
+              padding: EdgeInsets.only(top: size.height * 0.1),
               alignment: Alignment.topLeft,
               child: Image.asset(
                 'assets/images/SplashScreen3.png',
@@ -33,7 +56,7 @@ class SplashScreen extends StatelessWidget {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(bottom: size.height*0.1),
+              padding: EdgeInsets.only(bottom: size.height * 0.1),
               alignment: Alignment.bottomRight,
               child: Image.asset(
                 'assets/images/SplashScreen4.png',
@@ -41,14 +64,13 @@ class SplashScreen extends StatelessWidget {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: size.height*0.05),
+              padding: EdgeInsets.only(top: size.height * 0.05),
               alignment: Alignment.center,
               child: Image.asset(
                 'assets/images/logo.png',
                 height: size.height * 0.3,
               ),
             ),
-
           ],
         ),
       ),
